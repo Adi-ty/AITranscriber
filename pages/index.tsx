efabcc86a809d9f3dcad14da47cc3a60409d2ffa
Aditya <adityasinghboss1234@gmail.com>
2023-09-21 10:57:42 +0530

    feat(ui): 🎨Initial UI and form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,45 @@
 import Head from 'next/head'
-import StitchesLogo from '../components/StitchesLogo'
 import { styled } from '../stitches.config'
+import { LinkForm } from '../components/link-form'
+import { Output } from '../components/output'
 
 const Box = styled('div', {})
 
 const Text = styled('p', {
-  fontFamily: '$system',
-  color: '$hiContrast',
-})
-
-const Link = styled('a', {
-  fontFamily: '$system',
-  textDecoration: 'none',
-  color: '$purple600',
+    fontFamily: '$system',
+    color: '$hiContrast',
 })
 
 const Container = styled('div', {
-  marginX: 'auto',
-  paddingX: '$3',
+    marginX: 'auto',
+    paddingX: '$3',
 
-  variants: {
-    size: {
-      1: {
-        maxWidth: '300px',
-      },
-      2: {
-        maxWidth: '585px',
-      },
-      3: {
-        maxWidth: '865px',
-      },
+    variants: {
+        size: {
+            1: {
+                maxWidth: '300px',
+            },
+            2: {
+                maxWidth: '585px',
+            },
+            3: {
+                maxWidth: '865px',
+            },
+        },
     },
-  },
 })
 
 export default function Home() {
-  return (
-    <Box css={{ paddingY: '$6' }}>
-      <Head>
-        <title>Use Stitches with Next.js</title>
-      </Head>
-      <Container size={{ '@initial': '1', '@bp1': '2' }}>
-        <StitchesLogo />
-        <Text as="h1">Hello, from Stitches.</Text>
-        <Text>
-          For full documentation, visit{' '}
-          <Link href="https://stitches.dev">stitches.dev</Link>.
-        </Text>
-      </Container>
-    </Box>
-  )
+    return (
+        <Box css={{ paddingY: '$6' }}>
+            <Head>
+                <title>AI Transcriber</title>
+            </Head>
+            <Container size={{ '@initial': '1', '@bp1': '2' }}>
+                <Text as="h1">Translate your transcription with AI</Text>
+                <LinkForm />
+                <Output>Hello</Output>
+            </Container>
+        </Box>
+    )
 }
